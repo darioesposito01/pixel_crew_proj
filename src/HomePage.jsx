e890da77529f9d8fdde5a86d5f23c3b4262b7d8a
“Dario <d.esposito24@campus.unimib.it>
2024-07-23 13:51:30 +0200

    first commit

---
+++ src/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ArrowRight, Mail } from 'lucide-react';
+
+const SoulFarmHomepage = () => {
+  return (
+    <div></div>
+  );
+};
+
+export default SoulFarmHomepage;
